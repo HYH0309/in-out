5b8cee13cf26c19e6a823c9755b81f33ed544023
HYH0309 <155351289+HYH0309@users.noreply.github.com>
2025-02-03 12:01:55 +0800

    添加文件查看器组件，更新依赖项，配置 Unocss

---
+++ src/components/FileViewer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="file-viewer-container">
+        <textarea readonly :value="content" class="file-content-display"></textarea>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    content: String // 从父组件获取文件的内容
+});
+</script>
+
+<style scoped>
+.file-viewer-container {
+    @apply flex-1 p-4 box-border shadow-md rounded-lg border border-gray-300;
+}
+
+.file-content-display {
+    @apply w-full h-full bg-white text-black p-4 box-border outline-none rounded-lg;
+}
+</style>
